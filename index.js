990acb38810f9f8d35d20b583f20848c9ff73c0d
Nappa9693 <nerdyfreak9000@gmail.com>
2025-07-06 07:48:28 -0700

    Added functionality for multi-row spritesheets WIP

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ async function main() {
             input, output
         });
 
-        const srcDirectory = (await rl.question('Source Directory: '));
+        const srcDirectory = await rl.question('Source Directory: ');
         const destination = await rl.question('Destination: ');
+        const columnsCount = parseInt(await rl.question('Number of columns: '));
 
         // Get files to combine.
         const files = fs.readdirSync(`${srcDirectory}/`).sort();
-        console.log('Images found in specified directory: ', files, '...');
+        console.log(`${files.length } images found in specified directory...`);
 
         // Iterate through the images. First image will dictate the width
         // and height of each frame in the spritesheet and the dimensions
@@ -23,6 +24,11 @@ async function main() {
         let imageHeight = 0;
         let spritesheet = null;
 
+        let currentColumn = 0;
+        let currentRow = 0;
+        const rowCount = Math.round(files.length / columnsCount) + files.length % columnsCount
+
+        console.log(`Columns: ${columnsCount}, Calculated rows: ${rowCount}`);
         console.log('Combining images into spritesheet...');
         for(let i=0; i < files.length; i++) {
             const image = await Jimp.read(`${srcDirectory}/${files[i]}`);
@@ -31,15 +37,24 @@ async function main() {
                 imageWidth = image.width;
                 imageHeight = image.height;
                 spritesheet = new Jimp({
-                    width: files.length * imageWidth,
-                    height: imageHeight,
+                    width: imageWidth * columnsCount,
+                    height: imageHeight * rowCount,
                     color: 0x00000000
                 });
             }
 
-            const x = i * imageWidth;
-            const y = 0; // TODO: Change based on row.
+            if (currentColumn >= columnsCount) {
+                currentColumn = 0;
+                currentRow++;
+            } else if (i === files.length - 1 && files.length % columnsCount > 0) {
+                currentRow++;
+            }
+
+            const x = currentColumn * imageWidth;
+            const y = currentRow * imageHeight;
             spritesheet.composite(image, x, y);
+
+            currentColumn++;
         }
 
         await spritesheet.write(destination);
